17d4557953ed1fcef3bfe8197f94903d48c1aaf4
HenriUwU <henrisebille69@gmail.com>
2026-04-15 15:41:34 +0200

    chore: verify if file exists, if not, use thetas at 0

--- predict.py
+++ predict.py
@@ -1,3 +1,5 @@
+import os.path
+
 import numpy as np
 from colorama import Fore, Style
 
@@ -6,17 +8,21 @@ from train import estimate_price
 
 def main() -> None:
     try:
-        thetas = np.load("thetas.npz")
+        theta0 = 0
+        theta1 = 0
 
-        theta0 = thetas['theta0']
-        theta1 = thetas['theta1']
+        if os.path.exists("thetas.npz"):
+            thetas = np.load("thetas.npz")
+
+            theta0 = thetas['theta0']
+            theta1 = thetas['theta1']
 
         mileage = input(f"{Fore.LIGHTBLUE_EX}Your vehicle's Mileage : ")
 
         estimated_price = estimate_price(int(mileage), theta0, theta1)
 
-        if estimated_price > 0:
-            print(f"{Fore.GREEN}Estimated Price :"
+        if estimated_price >= 0:
+            print(f"{Fore.GREEN}Estimated Price : "
                   f"{estimated_price}{Style.RESET_ALL}")
         else:
             print(f"{Fore.GREEN}Unfortunately it seems like "
